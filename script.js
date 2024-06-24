31c5bced49805f4f534e6ae934fda416dca8621a
Harsha-0320 <bhondweharsha2010@gmail.com>
2024-06-24 23:36:11 +0530

    Updated commit

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-function sendEmail(){
-    Email.send({
-    Host: "smtp.gmail.com",
-    Username: "ahsh@gmail.com",
-    Password: "password",
-    To: 'reciever@gmail.com',
-    From: document.getElementById("email").value,
-    Subject: "Sending Email using javascript",
-    Body: "Well that was easy!!",
-})
-    .then(function (message) {
-        alert("mail sent successfully")
-    });
-}
+
 
 let menu = document.querySelector('#menu-bars');
 let header = document.querySelector('header');
