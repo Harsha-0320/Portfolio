2141c1c4599155693774e9b8ae8d6e93e46bf439
Harsha-0320 <bhondweharsha2010@gmail.com>
2024-06-27 21:30:12 +0530

    Updated commit

--- script.js
+++ script.js
@@ -10,27 +10,3 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     header.classList.remove('active');
 }
-
-function sendMail(){
-    var params = {
-        from_name : document.getElementById("name").value,
-        email : document.getElementById("email").value,
-        phone : document.getElementById("number").value,
-        message : document.getElementById("message").value,
-    };
-
-    const serviceID="service_ig31gys";
-    const templateID="template_p4py55e";
-
-    emailjs
-    .send(serviceID,templateID,params)
-    .then(function(res){
-        document.getElementById("name").value= "";
-        document.getElementById("email").value= "";
-        document.getElementById("number").value= "";
-        document.getElementById("message").value= "";
-        console.log(res);
-        alert("Your message sent successfully "+res.status);
-})
-.catch((err) => console.log(err));
-}
